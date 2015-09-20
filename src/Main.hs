73b84d6c6938d4caeccf49c083a968b99091a181
Taras Serdyuk <taras@techburg.io>
2015-09-21 02:22:31 +0300

    separate pub and sub servers

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,12 @@
 
 module Main where
 
+import Control.Concurrent (forkIO)
+import Data.ByteString as B (ByteString, tail)
 import Data.Maybe (fromMaybe)
-import Data.Text (Text)
 import Network.HTTP.Types (notFound404, ok200)
-import Network.Wai (Application, lazyRequestBody, pathInfo)
-import Network.Wai.Handler.Warp (defaultSettings, runSettings, setOnClose, setPort)
+import Network.Wai (Application, Request, lazyRequestBody, rawPathInfo)
+import Network.Wai.Handler.Warp (defaultSettings, run, runSettings, setOnClose, setPort)
 import ReadArgs (readArgs)
 import SSE (SSErvice, bsEvent, emptyResponse, onClose, send, sservice, subscribe)
 
@@ -13,13 +14,18 @@ import SSE (SSErvice, bsEvent, emptyResponse, onClose, send, sservice, subscribe
 main :: IO ()
 main = do
 	sse <- sservice
-	port <- fmap (fromMaybe 3000) readArgs
-	let settings = setPort port $ setOnClose (onClose sse) defaultSettings
-	runSettings settings (application sse)
+	(subPort, pubPort) <- readArgs
+	let subSettings = setPort (fromMaybe 3000 subPort) $
+		setOnClose (onClose sse) defaultSettings
+	forkIO $ runSettings subSettings (subscriber sse)
+	run (fromMaybe 5000 pubPort) (publisher sse)
 
-application :: SSErvice Text -> Application
-application sse request respond = return (pathInfo request) >>= \case
-	["publish", key] -> lazyRequestBody request >>=
-		(send sse key . bsEvent) >> respond (emptyResponse ok200)
-	["subscribe", key] -> subscribe sse key request respond
-	_ -> respond (emptyResponse notFound404)
+subscriber :: SSErvice ByteString -> Application
+subscriber sse request = subscribe sse (key request) request
+
+publisher :: SSErvice ByteString -> Application
+publisher sse request respond = lazyRequestBody request >>=
+	(send sse (key request) . bsEvent) >> respond (emptyResponse ok200)
+
+key :: Request -> ByteString
+key = B.tail . rawPathInfo
